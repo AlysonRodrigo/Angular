a8bbdb72a16e4b77bd789584190bcef3ca1e070a
AlysonRodrigo <alysonrodrigo14@hotmail.com>
2021-09-27 13:43:33 -0300

    criando interface de entrar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
 import { RodapeComponent } from './rodape/rodape.component';
 import { EnterComponent } from './enter/enter.component';
+import { CadastroComponent } from './cadastro/cadastro.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     MenuComponent,
     RodapeComponent,
-    EnterComponent
+    EnterComponent,
+    CadastroComponent
   ],
   imports: [
     BrowserModule,
