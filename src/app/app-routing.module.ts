940bc03653a9902e733fffd3cb229db247adf620
AlysonRodrigo <alysonrodrigo14@hotmail.com>
2021-09-27 22:04:31 -0300

    area de login e cadastro criados

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { CadastroComponent } from './cadastro/cadastro.component';
+import { EnterComponent } from './enter/enter.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  
+  {path: '', redirectTo:'enter', pathMatch: 'full'},
+  {path:"enter", component: EnterComponent},
+  {path:"cadastro",component:CadastroComponent}
+  
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
